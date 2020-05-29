c9bdfb18fd771a9aea0233d85efbcfecdd6cddd2
kartik-soni1707 <its.kartiksoni@gmail.com>
2020-05-29 12:57:06 +0530

    Cross Val

--- SVM.py
+++ SVM.py
@@ -6,16 +6,22 @@ import nltk
 import string
 import re
 from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer,HashingVectorizer
+#from sklearn.svm import SVC as clf
+from sklearn.ensemble import RandomForestClassifier 
 from sklearn.svm import SVC as clf
+from sklearn.model_selection import cross_val_score
+
 #Extracting the data
-re.compile('<title>(.*)</title>')
+
 df=pd.read_csv("sms.csv").iloc[1:,1:]
 v=CountVectorizer()
 x = v.fit_transform(df.iloc[:,0].values.astype('U'))  ## Even astype(str) would work
 y=df.iloc[:,1].values
+classifier=clf()
+scores = cross_val_score(classifier, x, y, cv=10)
+print(sum(scores)/len(scores))
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test=train_test_split(x,y,test_size=0.1)
-classifier=clf(kernel='poly',degree=1)
 classifier.fit(X_train,y_train)
 predRF=classifier.predict(X_test)
 predict=[]
@@ -37,3 +43,5 @@ print('Recall score: {}'.format(recall_score(real, predict)))
 print('F1 score: {}'.format(f1_score(real, predict)))
 cm=confusion_matrix(real, predict)
 print(cm)
+
+
